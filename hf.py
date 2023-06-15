063143c3e65afd6a63cbeadef19d98e8e1acc096
Cody Sterling <cody.sterling@protonmail.com>
2023-06-15 15:39:28 +0200

    rewrote matrix fill loop, checked HeH+ consistency

--- hf.py
+++ hf.py
@@ -2,14 +2,13 @@
 
 import sys
 import numpy as np
-import scipy as sp
-from scipy.special import erf
+import math
 
 ######################
 # Defining functions #
 ######################
 
-def read_xyz(file):
+def readXYZ(file):
 	atoms = []
 	coords = []
 
@@ -46,9 +45,9 @@ def F0(t):
 	if t == 0:
 		return 1
 	else:
-		return (0.5)*(np.pi/t)**0.5*erf(t**0.5)
+		return (0.5)*(np.pi/t)**0.5*math.erf(t**0.5)
 
-def ne_pot(A,B,i):
+def nePotential(A,B,i):
 	p, sqdiff, K, Rp = gProd(A,B)
 	Rc = coords[i]
 	Zc = charges[atoms[i]]
@@ -66,7 +65,7 @@ def diff(P,Pt):
 			x += ((Pt[i,j]-P[i,j])/basis_size)**2
 	return x**0.5
 
-def nuc_repul():
+def nuclearRepulsion():
 	nuc_rep = 0
 	for i,A in enumerate(atoms):
 		print("i is {} {}".format(i,A))
@@ -83,15 +82,18 @@ def nuc_repul():
 			nuc_rep += (charge_A*charge_B)/R
 	return 0.5*nuc_rep
 
-######################
-
-natoms, atoms, coords = read_xyz(sys.argv[1])
-#natoms, atoms, coords = read_xyz("/home/cody/Documents/Gitlab/hf-python/xyz-files/heh.xyz")
+natoms, atoms, coords = readXYZ(sys.argv[1])
+#natoms, atoms, coords = readXYZ("/home/cody/Documents/Gitlab/hf-python/xyz-files/heh.xyz")
 
 zetas = {'H': [1.24], 'He': [2.0925], 'Li': [2.69, 0.75], 'Be': [3.68, 1.10], 'B': [4.68, 1.45, 1.45, 1.45, 1.45], 'C': [5.67, 1.72, 1.72, 1.72, 1.72], 'N': [6.67, 1.95, 1.95, 1.95, 1.95], 'O': [7.66, 2.25, 2.25, 2.25, 2.25], 'F': [8.65, 2.55, 2.55, 2.55, 2.55]}
 
-# changed He zeta to match ORCA?
-#zetas = {'H': [1.24], 'He': [1.6875], 'Li': [2.69, 0.75], 'Be': [3.68, 1.10], 'B': [4.68, 1.45, 1.45, 1.45, 1.45], 'C': [5.67, 1.72, 1.72, 1.72, 1.72], 'N': [6.67, 1.95, 1.95, 1.95, 1.95], 'O': [7.66, 2.25, 2.25, 2.25, 2.25], 'F': [8.65, 2.55, 2.55, 2.55, 2.55]}
+# matching ORCA
+#zetas = {'H': [1.24], 'He': [2.0925], 'Li': [2.69, 0.8, 0.8], 'Be': [3.68, 1.10], 'B': [4.68, 1.45, 1.45, 1.45, 1.45], 'C': [5.67, 1.72, 1.72, 1.72, 1.72], 'N': [6.67, 1.95, 1.95, 1.95, 1.95], 'O': [7.66, 2.25, 2.25, 2.25, 2.25], 'F': [8.65, 2.55, 2.55, 2.55, 2.55]}
+
+basis_size = 0
+for atom in atoms:
+	basis_size += len(zetas[atom])
+print('Basis size: {}'.format(basis_size))
 
 # STO-1G for H
 #d_list = np.array([[1.0]])
@@ -107,21 +109,13 @@ a_list = np.array([[0.109818, 0.405771, 2.22766], [0.0751386, 0.231031, 0.994203
 
 # Atom charges
 charges = {'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9}
+
 n_elec = 0
 charge = 0
 for atom in atoms:
-	n_elec += charges.get(atom)
+	n_elec += charges[atom]
 n_elec -= charge
 print('nelec: {}'.format(n_elec))
-
-basis_size = 0
-for atom in atoms:
-#	basis_size += len(np.unique(zetas[atom]))
-	basis_size += len(zetas[atom])
-print('Basis size: {}'.format(basis_size))
-
-
-
 
 # Making F matrices for overlap (S), kinetic energies (T), and potential energies (V), and multi-electron tensor (M)
 S = np.zeros((basis_size,basis_size))
@@ -129,83 +123,103 @@ T = np.zeros((basis_size,basis_size))
 V = np.zeros((basis_size,basis_size))
 M = np.zeros((basis_size,basis_size,basis_size,basis_size))
 
-for i,atom in enumerate(atoms):
-
+a = 0
+for i,atomA in enumerate(atoms):
 	# Get atom charge and coordinates for centering the basis functions
-	Za = charges[atom]
+	Za = charges[atomA]
 	Ra = np.array(coords[i])
-	print('For atom {} {} we have Za = {} and Ra = {}'.format(i,atom,Za,Ra))
+	print('For atom {} {} we have Za = {} and Ra = {}'.format(i,atomA,Za,Ra))
 
 	# Iterating through quantum numbers on site A (1s, 2s, etc.)
-	for qA in range(len(zetas[atom])):
-		d_A = d_list[qA]
-		zeta_Aq = zetas[atom][qA]
-		a_A = a_list[qA]*zeta_Aq**2
-		print('qA is {}, d_A = {}, zeta_Aq = {}, a_A = {}'.format(qA,d_A,zeta_Aq,a_A))
+	for qA,zeta_qA in enumerate(zetas[atomA]):
+		b = 0
+		d_qA = d_list[qA]
+		a_qA = a_list[qA]*zeta_qA**2
+		print('qA is {}, d_A = {}, zeta_Aq = {}, a_A = {}'.format(qA,d_qA,zeta_qA,a_qA))
 
-		for lA in range(len(d_A)): # Iterating over contracted Gaussians for A
-			# Iterating through B atoms
-			for j,atomB in enumerate(atoms):
-				Zb = charges[atomB]
-				Rb = np.array(coords[j])
-				print('For atom {} {} we have Zb = {} and Rb = {}'.format(j,atomB,Zb,Rb))
+		# Iterating through B atoms
+		for j,atomB in enumerate(atoms):
+			
+			Zb = charges[atomB]
+			Rb = np.array(coords[j])
+			print('For atom {} {} we have Zb = {} and Rb = {}'.format(j,atomB,Zb,Rb))
 
-				# Iterating through quantum numbers on site B (1s, 2s, etc.)
-				for qB in range(len(zetas[atomB])):
-					d_B = d_list[qB]
-					zeta_Bq = zetas[atomB][qB]
-					a_B = a_list[qB]*zeta_Bq**2
-					print('d_B = {}, zeta_Bq = {}, a_B = {}'.format(d_B,zeta_Bq,a_B))
+			# Iterating through quantum numbers on site B (1s, 2s, etc.)
+			for qB,zeta_qB in enumerate(zetas[atomB]):
+				c = 0
+				d_qB = d_list[qB]
+				a_qB = a_list[qB]*zeta_qB**2
+				print('qB is {}, d_B = {}, zeta_Bq = {}, a_B = {}'.format(qB,d_qB,zeta_qB,a_qB))
 
-					for lB in range(len(d_B)): # Iterating over contracted Gaussians for B
-						a = (i+1)*(qA+1)-1
-						b = (j+1)*(qB+1)-1
+				for lA in range(len(d_qA)): # Iterating over contracted Gaussians for A
+					for lB in range(len(d_qB)): # Iterating over contracted Gaussians for B
+						# a = (i+1)*(qA+1)-1
+						# b = (j+1)*(qB+1)-1
 						print(' ! i: {}, qA: {}, lA: {}, j: {}, qB: {}, lB: {}'.format(i,qA,lA,j,qB,lB))
 						print(' ! a: {} b: {}'.format(a,b))
 
-						S[a,b] += d_A[lA]*d_B[lB]*overlap((a_A[lA],Ra),(a_B[lB],Rb))
-						T[a,b] += d_A[lA]*d_B[lB]*kinetic((a_A[lA],Ra),(a_B[lB],Rb))
+						S[a,b] += d_qA[lA]*d_qB[lB]*overlap((a_qA[lA],Ra),(a_qB[lB],Rb))
+						T[a,b] += d_qA[lA]*d_qB[lB]*kinetic((a_qA[lA],Ra),(a_qB[lB],Rb))
 
 						for n in range(natoms):
-							V[a,b] += d_A[lA]*d_B[lB]*ne_pot((a_A[lA],Ra),(a_B[lB],Rb),n)
+							V[a,b] += d_qA[lA]*d_qB[lB]*nePotential((a_qA[lA],Ra),(a_qB[lB],Rb),n)
 
-						for k,atomC in enumerate(atoms): # Going through C and D to make the M tensor
-							Zc = charges[atomC]
-							Rc = np.array(coords[k])
-							print('For atom {} {} we have Zc = {} and Rc = {}'.format(k,atomC,Zc,Rc))
+				# Going through C and D to make the M tensor
+				for k,atomC in enumerate(atoms):
+					Zc = charges[atomC]
+					Rc = np.array(coords[k])
+					print('For atom {} {} we have Zc = {} and Rc = {}'.format(k,atomC,Zc,Rc))
 
-							# Iterating through quantum numbers on site C (1s, 2s, etc.)
-							for qC in range(len(zetas[atomC])):
-								d_C = d_list[qC]
-								zeta_Cq = zetas[atomC][qC]
-								a_C = a_list[qC]*zeta_Cq**2
-								print('d_C = {}, zeta_Cq = {}, a_C = {}'.format(d_C,zeta_Cq,a_C))
+					# Iterating through quantum numbers on site C (1s, 2s, etc.)
+					for qC,zeta_qC in enumerate(zetas[atomC]):
+						d = 0
+						d_qC = d_list[qC]
+						a_qC = a_list[qC]*zeta_qC**2
+						print('d_C = {}, zeta_Cq = {}, a_C = {}'.format(d_qC,zeta_qC,a_qC))
 
-								for lC in range(len(d_C)): # Iterating over contracted Gaussians for C
+						for m,atomD in enumerate(atoms):
+							Zd = charges[atomD]
+							Rd = np.array(coords[m])
+							print('For atom {} {} we have Zd = {} and Rd = {}'.format(m,atomD,Zd,Rd))
 
-									for m,atomD in enumerate(atoms):
-										Zd = charges[atomD]
-										Rd = np.array(coords[m])
-										print('For atom {} {} we have Zd = {} and Rd = {}'.format(m,atomD,Zd,Rd))
+							# Iterating through quantum numbers on site D (1s, 2s, etc.)
+							for qD,zeta_qD in enumerate(zetas[atomD]):
+								d_qD = d_list[qD]
+								a_qD = a_list[qD]*zeta_qD**2
+								print('d_D = {}, zeta_Dq = {}, a_D = {}'.format(d_qD,zeta_qD,a_qD))
 
-										# Iterating through quantum numbers on site D (1s, 2s, etc.)
-										for qD in range(len(zetas[atomD])):
-											d_D = d_list[qD]
-											zeta_Dq = zetas[atomD][qD]
-											a_D = a_list[qD]*zeta_Dq**2
-											print('d_D = {}, zeta_Dq = {}, a_D = {}'.format(d_D,zeta_Dq,a_D))
-
-											for lD in range(len(d_D)): # Iterating over contracted Gaussians for D
-												c = (k+1)*(qC+1)-1
-												d = (m+1)*(qD+1)-1
+								for lA in range(len(d_qA)): # Iterating over contracted Gaussians for A
+									for lB in range(len(d_qB)): # Iterating over contracted Gaussians for B
+										for lC in range(len(d_qC)): # Iterating over contracted Gaussians for C
+											for lD in range(len(d_qD)): # Iterating over contracted Gaussians for D
+												# c = (k+1)*(qC+1)-1
+												# d = (m+1)*(qD+1)-1
 												print(' ! k: {}, qC: {}, lC: {}, m: {}, qD: {}, lD: {}'.format(k,qC,lC,m,qD,lD))
 												print(' ! c: {} d: {}'.format(c,d))
-												M[a,b,c,d] += d_A[lA]*d_B[lB]*d_C[lC]*d_D[lD]*abcd((a_A[lA],Ra),(a_B[lB],Rb),(a_C[lC],Rc),(a_D[lD],Rd))
+												M[a,b,c,d] += d_qA[lA]*d_qB[lB]*d_qC[lC]*d_qD[lD]*abcd((a_qA[lA],Ra),(a_qB[lB],Rb),(a_qC[lC],Rc),(a_qD[lD],Rd))
+
+								
+								d += 1
+								print('hered {}'.format(d))
+						
+						c += 1
+						print('herec {}'.format(c))
+				
+				b += 1
+				print('hereb {}'.format(b))
+		
+		a += 1
+		print('herea {}'.format(a))
+
 
 print('S:\n{}\n'.format(S))
+print('Ssum: {}'.format(np.sum(S)))
 print('T:\n{}\n'.format(T))
+print('Tsum: {}'.format(np.sum(T)))
 print('V:\n{}\n'.format(V))
+print('Vsum: {}'.format(np.sum(V)))
 print('M:\n{}\n'.format(M))
+print('Msum: {}'.format(np.sum(M)))
 
 Hcore = T + V
 
@@ -264,14 +278,14 @@ while threshold > 1e-6:
 	for i in range(basis_size):
 		for j in range(basis_size):
 			for a in range(int(n_elec/2)):
-				print('Cia = C{}{} = {}'.format(i,a,C[i,a]))
-				print('Cja = C{}{} = {}'.format(j,a,C[j,a]))
+				print('Cia = C{}{} = {:0.5f}'.format(i,a,C[i,a]))
+				print('Cja = C{}{} = {:0.5f}'.format(j,a,C[j,a]))
 				P[i,j] = 2*C[i,a]*C[j,a]
 	print('P:\n{}\n'.format(P))
 
 	P_list.append(P)
 	threshold = diff(P,Pt)
-	print('Threshold is {}'.format(threshold))
+	print('Threshold is {:0.5f}'.format(threshold))
 	Pt = P.copy()
 
 	# Calculate energy
@@ -281,7 +295,7 @@ while threshold > 1e-6:
 			print("u, v: {}, {}".format(u,v))
 			E0 += 0.5*P[v,u]*(Hcore[u,v] + F[u,v])
 	E_list.append(E0)
-	print('Energy: {}'.format(E0))
+	print('Energy: {:0.5f}'.format(E0))
 
 print("Converged in {} iterations".format(len(P_list)))
 print("Orbital energies are {}".format(evalFprime))
@@ -289,5 +303,5 @@ print("Orbital matrix is\n  {}".format(C))
 print("Density bond order matrix is\n  {}".format(P))
 print("Energy is {}".format(E_list[-1]))
 print("Energy list is {}".format(E_list))
-print("Nuclear repulsion is {}".format(nuc_repul()))
-print("Total energy is {}".format(E_list[-1]+nuc_repul()))
+print("Nuclear repulsion is {}".format(nuclearRepulsion()))
+print("Total energy is {}".format(E_list[-1]+nuclearRepulsion()))
